36030de32dc151e41b4558a2046fbdb7b1535aed
Aarsh Mishra <aarshmail@gmail.com>
2025-04-14 00:19:22 +0530

    added files and details

--- components/client-wrappers.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import dynamic from "next/dynamic"
-
-// Dynamic imports with ssr: false
-export const CustomCursorClient = dynamic(() => import("@/components/custom-cursor"), { ssr: false })
-export const ThreeCanvasClient = dynamic(() => import("@/components/three/three-canvas"), { ssr: false })
-export const BriefcaseIconClient = dynamic(() => import("@/components/three/briefcase-icon"), { ssr: false })
-export const ProfileMeshClient = dynamic(() => import("@/components/three/profile-mesh"), { ssr: false })
